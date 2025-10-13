2a02077f0ab90d30a53889262fcebe2def17e0e3
Jake <morgan.jake888@gmail.com>
2025-10-13 17:18:37 +0100

    Fixed bug preventing multiple expressions on the same line
    Bug in the code meant that after an expression was ran the interpreter would go to the next line regardless

--- language/main.c
+++ language/main.c
@@ -13,6 +13,9 @@
 #include "Parser/Parser.c"
 #include "../debugging/CodeViewer.c"
 
+// Maximum length of a single line
+#define MaxLineBufferSize 1024
+
 double GetTime() {
 	static LARGE_INTEGER Frequency;
     static int Initialized = 0;
@@ -27,9 +30,6 @@ double GetTime() {
     return (double) Counter.QuadPart * 1000.0 / (double)Frequency.QuadPart;
 }
 
-// Maximum length of a single line
-#define MaxLineBufferSize 1024
-
 struct Value RunLine(char* LineChars, struct Variable** Variables, int* VariableCount) {
 
 	struct Tokens* Tokens = Tokenize(LineChars);
@@ -43,13 +43,13 @@ struct Value RunLine(char* LineChars, struct Variable** Variables, int* Variable
 	return Parse(LineChars, AST, ASTNodeCount, Variables, VariableCount);
 }
 
-int main(void) {
+void RunProgram() {
 	
 	FILE *File = fopen("../code/main.txt", "r");
 
 	if (File == NULL) {
 		printf("No main file found in code folder\n");
-		return 0;
+		return;
 	}
 
 	double StartProgramTime = GetTime();
@@ -62,11 +62,11 @@ int main(void) {
     struct Variable* Variables = malloc(0);
     int VariableCount = 0;
 
+	char* FileChars = malloc(0);
+	GlobalLine = malloc(0);
+
 	// Iterates over the lines and adds it to a single array
 	while(fgets(LineBuffer, MaxLineBufferSize, File)) {
-
-		char* FileChars = malloc(0);
-		GlobalLine = malloc(0);
 
 		for (int i = 0; i < MaxLineBufferSize; i++) {
 			
@@ -79,6 +79,7 @@ int main(void) {
 			BufferLocation++;
 
 			if (LineBuffer[i] == 59 || LineBuffer[i] == 123) {
+				printf("'%s'\n", FileChars);
 				struct Value Value = RunLine(FileChars, &Variables, &VariableCount);
 				free(FileChars);
 				free(GlobalLine);
@@ -108,7 +109,8 @@ int main(void) {
 					}
 				}
 
-				break;
+				FileChars = malloc(0);
+				GlobalLine = malloc(0);
 			}
 			
 			if (LineBuffer[i] == 10 || LineBuffer[i] == 0) {
@@ -119,6 +121,13 @@ int main(void) {
 
 	printf("Program ran in: %.4f ms\n", GetTime() - StartProgramTime);
 	fclose(File);
+}
+
+int main(void) {
+	
+	for (int i = 0; i < 1; i++) {
+		RunProgram();
+	}
 
 	return 0;
 }
